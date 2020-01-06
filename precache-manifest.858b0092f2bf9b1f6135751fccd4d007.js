29e3c4eda40656190008bf57ab426f30671c7953
Mohsen Garousi <44213866+MohsenGarousi@users.noreply.github.com>
2020-01-07 02:47:15 +0330

    Updates

---
+++ precache-manifest.858b0092f2bf9b1f6135751fccd4d007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46919ee9a2373b43d05e77135ce254f2",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "56a1b34c60e22dac700d",
+    "url": "/MyResume/static/css/main.c6d06803.chunk.css"
+  },
+  {
+    "revision": "cdb2970d90db57e3a8de",
+    "url": "/MyResume/static/js/2.2a708489.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MyResume/static/js/2.2a708489.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56a1b34c60e22dac700d",
+    "url": "/MyResume/static/js/main.34d6a11b.chunk.js"
+  },
+  {
+    "revision": "2d3663d1c3334291b0fa",
+    "url": "/MyResume/static/js/runtime-main.c8432e9f.js"
+  }
+]);
